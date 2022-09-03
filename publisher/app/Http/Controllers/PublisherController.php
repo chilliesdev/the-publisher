4b460cd7c229e121d57eb9a93a707947a056a40f
MatthewKayodeMatthew <kayode@spaceuniverse.africa>
2022-09-03 18:17:30 +0100

    corrected controller name

--- publisher/app/Http/Controllers/PublisherController.php
+++ publisher/app/Http/Controllers/PublisherController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class Publisher extends Controller
+class PublisherController extends Controller
 {
     public function subscribe(Request $request, $topic)
     {
