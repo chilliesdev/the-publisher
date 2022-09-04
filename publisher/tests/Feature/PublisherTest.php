d1593dd2144d026963982ac19a6a28d926ca30e5
MatthewKayodeMatthew <kayode@spaceuniverse.africa>
2022-09-04 21:12:38 +0100

    Created list of subscribers CRUD

---
+++ publisher/tests/Feature/PublisherTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PublisherTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_subscribe()
+    {
+        $response = $this->post('/api/subscribe/new-topic', [
+            "url" => "http://mysubscriber.test/"
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
---
+++ publisher/tests/Feature/SubscribersTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SubscribersTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    public function test_get_all_subscribers()
+    {
+        $response = $this->get('/api/subscribers');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_create_new_subscriber(): string
+    {
+        $response = $this->post('/api/subscribers', [
+            "url" => "http://mysubscriber.test/"
+        ]);
+        
+        $data = json_decode($response->getContent(), true);
+
+        $id = $data['id'];
+
+        $response->assertStatus(201);
+
+        return $id;
+    }
+
+    /**
+     * @depends test_create_new_subscriber
+     */
+
+    public function test_update_subscriber(string $id): void
+    {
+        $response = $this->patch('/api/subscribers/'.$id, [
+            "url" => "http://mysubscriber.test/"
+        ]);
+
+        dd($response);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @depends test_create_new_subscriber
+     */
+
+    public function test_delete_subscriber(string $id): void
+    {
+        $response = $this->delete('/api/subscribers/'.$id);
+
+        $response->assertStatus(200);
+    }
+}
